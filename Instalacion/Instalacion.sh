fcc29ebc5df34363b44da2ab44d437f3693caf0a
AndresFlorez-Git <af.florez12@uniandes.edu.co>
2022-06-11 15:43:33 -0500

    Intalacion tensroflow

--- Instalacion/Instalacion.sh
+++ Instalacion/Instalacion.sh
@@ -4,6 +4,8 @@ sudo apt-get install python3-picamera
 sudo apt-get update
 sudo apt-get upgrade
 
+
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_armv7l.whl
 python3 -m pip uninstall tensorflow
 python3 -m pip install tensorflow-2.4.0-cp37-none-linux_armv7l.whl
 
